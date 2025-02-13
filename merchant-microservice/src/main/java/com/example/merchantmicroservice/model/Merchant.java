7c0039f9c5a7049d977f9426a65f0842836deb70
MangelHurtado <miguelangel.hurtado@petroprix.com>
2025-02-13 12:06:11 +0100

    feat: [MODEL]: Agregar entidad Merchant

---
+++ merchant-microservice/src/main/java/com/example/merchantmicroservice/model/Merchant.java
@@ -0,0 +1,41 @@
+package com.example.merchantmicroservice.model;
+
+import lombok.*;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbIgnore;
+
+@DynamoDbBean
+@EqualsAndHashCode(callSuper=false)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class Merchant extends MainTable{
+
+    public static final String MERCHANT_PK_PREFIX = "MERCHANT#";
+    public static final String MERCHANT_SK_PREFIX = "MERCHANT#";
+
+    private String name;
+    private String address;
+    private Merchant merchantType;
+
+    public void setId(String id) {
+        setPartitionKey(MerchantKeyBuilder.makePartitionKey(id));
+        setSortKey(MerchantKeyBuilder.makeSortKey());
+    }
+
+    @DynamoDbIgnore
+    public String getId() { return getPartitionKey().substring(MERCHANT_PK_PREFIX.length()); }
+
+    public static class MerchantKeyBuilder {
+        private MerchantKeyBuilder() {
+        }
+
+        public static String makePartitionKey(String id) { return MERCHANT_PK_PREFIX + id; }
+
+        public static String makeSortKey(){ return MERCHANT_SK_PREFIX; }
+
+    }
+}
