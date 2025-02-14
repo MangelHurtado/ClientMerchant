273d49213702b3523c615d93f782c67ef360872c
MangelHurtado <miguelangel.hurtado@petroprix.com>
2025-02-14 11:09:41 +0100

    feat: [DYNAMODB]: Añadir configuración para dynamodb en Client

---
+++ client-microservice/src/main/java/com/example/clientmicroservice/config/DynamoDBConfig.java
@@ -0,0 +1,41 @@
+package com.example.clientmicroservice.config;
+
+
+import com.example.clientmicroservice.utils.AwsCredentials;
+import com.example.clientmicroservice.utils.AwsProperties;
+import lombok.var;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
+
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties({AwsCredentials.class, AwsProperties.class})
+public class DynamoDBConfig {
+
+    @Bean
+    public DynamoDbClient dynamoDbClient(AwsProperties properties,
+                                         AwsBasicCredentials awsCredentials) {
+        var builder = DynamoDbClient.builder().region(Region.of(properties.getRegion()));
+        if (properties.getEndpointOverride() != null) {
+            builder.endpointOverride(URI.create(properties.getEndpointOverride()));
+        }
+        return builder.credentialsProvider(() -> awsCredentials).build();
+    }
+
+    @Bean
+    public AwsBasicCredentials awsCredentials(AwsProperties properties) {
+        return AwsBasicCredentials.create(properties.getCredentials().getAccessKey(),
+                properties.getCredentials().getSecretKey());
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient).build();
+    }
+}
---
+++ client-microservice/src/main/java/com/example/clientmicroservice/utils/AwsProperties.java
@@ -0,0 +1,23 @@
+package com.example.clientmicroservice.utils;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConfigurationProperties("aws")
+@ConstructorBinding
+public class AwsProperties {
+    private final String endpointOverride;
+    private final String region;
+    private final String dynamoDbTableName;
+    private final AwsCredentials credentials;
+
+    public AwsProperties(String endpointOverride, String region, String dynamoDbTableName, AwsCredentials credentials) {
+        this.endpointOverride = endpointOverride;
+        this.region = region;
+        this.dynamoDbTableName = dynamoDbTableName;
+        this.credentials = credentials;
+    }
+}
+
