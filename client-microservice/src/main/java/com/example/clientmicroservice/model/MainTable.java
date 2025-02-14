4090d3b4c73c51bbc65882f5306e454bf21ca54c
MangelHurtado <miguelangel.hurtado@petroprix.com>
2025-02-14 11:12:50 +0100

    feat: [MAINTABLE]: Agregar la tabla para la dynamodb en Client

---
+++ client-microservice/src/main/java/com/example/clientmicroservice/model/MainTable.java
@@ -0,0 +1,39 @@
+package com.example.clientmicroservice.model;
+
+import lombok.Data;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+import java.time.Instant;
+
+@Data
+public class MainTable {
+
+    protected String partitionKey;
+    protected String sortKey;
+    protected String id;
+    protected String status;
+    protected String gIndex2Pk;
+    protected Instant createdDate;
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("PK")
+    public String getPartitionKey() { return partitionKey; }
+
+    @DynamoDbSortKey
+    @DynamoDbAttribute("SK")
+    public String getSortKey() { return sortKey; }
+
+    @DynamoDbAttribute("id")
+    public String getId() { return id; }
+
+    @DynamoDbAttribute("status")
+    public String getStatus() { return status; }
+
+    @DynamoDbAttribute("gIndex2Pk")
+    public String getGIndex2Pk() { return gIndex2Pk; }
+
+    @DynamoDbAttribute("createdDate")
+    public Instant getCreatedDate() { return createdDate; }
+}
