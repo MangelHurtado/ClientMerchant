8e8743e84b1d32849b429172f838b54ebd9a4f1a
MangelHurtado <miguelangel.hurtado@petroprix.com>
2025-03-04 10:30:02 +0100

    feat: [LAYOUT]: Añadir el layout base de la aplicación

---
+++ frontend/src/common/components/Delivery/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react"
+
+const Example: FC = () => {
+  return <>Example</>
+}
+
+export default Example
--- frontend/src/common/components/ExampleComponent/Delivery/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react"
-
-const ExampleComponent: FC = () => {
-  return <>Esto es un ejemplo</>
-}
-
-export default ExampleComponent
